3e9ec73deda9a116991cb0b584c54753c4ece200
lerf <lerf@ug4c31.windows.cin.ufpe.br>
2026-05-26 09:58:06 -0300

    Como baixar o arquivo de HIDS

---
+++ scripts/prepare_hids.py
@@ -0,0 +1,19 @@
+import pyarrow.parquet as pq
+import pandas as pd
+
+pf = pq.ParquetFile("process_uber_summary.parquet")
+
+target = 1_000_000
+batches = []
+read = 0
+
+for rg in range(pf.num_row_groups):
+    table = pf.read_row_group(rg)   # pode passar columns=[...]
+    batches.append(table)
+    read += table.num_rows
+    if read >= target:
+        break
+
+df = table = None
+df = pd.concat([t.to_pandas() for t in batches], ignore_index=True).head(target)
+df.to_csv("process_uber_summary.csv", index=False)
