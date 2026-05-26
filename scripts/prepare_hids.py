7350fb724b5a08b3759b75d0b299db28d09fbeb5
renanrv4 <renan.santana.arv@gmail.com>
2026-05-26 20:23:53 -0300

    Consertando erros de configuração (linting) e dependência faltante

--- scripts/prepare_hids.py
+++ scripts/prepare_hids.py
@@ -1,5 +1,5 @@
-import pyarrow.parquet as pq
 import pandas as pd
+import pyarrow.parquet as pq
 
 pf = pq.ParquetFile("process_uber_summary.parquet")
 
@@ -8,7 +8,7 @@ batches = []
 read = 0
 
 for rg in range(pf.num_row_groups):
-    table = pf.read_row_group(rg)   # pode passar columns=[...]
+    table = pf.read_row_group(rg)  # pode passar columns=[...]
     batches.append(table)
     read += table.num_rows
     if read >= target:
--- scripts/prepare_ugr16.py
+++ scripts/prepare_ugr16.py
@@ -14,33 +14,22 @@ cols = [
     "tos",
     "packets",
     "bytes",
-    "label"
+    "label",
 ]
 
 print("Lendo dataset...")
 
 # Extraindo apenas 1000000 de linhas do dataset original por causa do tamanho do arquivo
-df = pd.read_csv(
-    "data/ugr16/march.week3.csv",
-    header=None,
-    names=cols,
-    nrows=1_000_000
-)
+df = pd.read_csv("data/ugr16/march.week3.csv", header=None, names=cols, nrows=1_000_000)
 
 print("Convertendo timestamps...")
 
 # Timestamp -> epoch milliseconds
-df["FLOW_START_MILLISECONDS"] = (
-    pd.to_datetime(df["timestamp"]).astype("int64") // 10**6
-)
+df["FLOW_START_MILLISECONDS"] = pd.to_datetime(df["timestamp"]).astype("int64") // 10**6
 
 print("Convertendo protocolos...")
 
-proto_map = {
-    "ICMP": 1,
-    "TCP": 6,
-    "UDP": 17
-}
+proto_map = {"ICMP": 1, "TCP": 6, "UDP": 17}
 
 df["PROTOCOL"] = df["protocol"].map(proto_map)
 
@@ -60,30 +49,29 @@ df["Label"] = (df["label"] != "background").astype(int)
 print("Montando dataframe final...")
 
 # DataFrame final com todas as colunas convertidas
-final_df = pd.DataFrame({
-    "IPV4_SRC_ADDR": df["src_ip"],
-    "IPV4_DST_ADDR": df["dst_ip"],
-    "FLOW_START_MILLISECONDS": df["FLOW_START_MILLISECONDS"],
-    "DURATION": df["duration"],
-    "L4_SRC_PORT": df["src_port"],
-    "L4_DST_PORT": df["dst_port"],
-    "PROTOCOL": df["PROTOCOL"],
-    "TCP_FLAGS": df["TCP_FLAGS"],
-    "FWD": df["fwd"],
-    "TOS": df["tos"],
-    "IN_PKTS": df["packets"],
-    "IN_BYTES": df["bytes"],
-    "Attack": df["Attack"],
-    "Label": df["Label"]
-})
+final_df = pd.DataFrame(
+    {
+        "IPV4_SRC_ADDR": df["src_ip"],
+        "IPV4_DST_ADDR": df["dst_ip"],
+        "FLOW_START_MILLISECONDS": df["FLOW_START_MILLISECONDS"],
+        "DURATION": df["duration"],
+        "L4_SRC_PORT": df["src_port"],
+        "L4_DST_PORT": df["dst_port"],
+        "PROTOCOL": df["PROTOCOL"],
+        "TCP_FLAGS": df["TCP_FLAGS"],
+        "FWD": df["fwd"],
+        "TOS": df["tos"],
+        "IN_PKTS": df["packets"],
+        "IN_BYTES": df["bytes"],
+        "Attack": df["Attack"],
+        "Label": df["Label"],
+    }
+)
 
 print(final_df.head())
 
 print("Salvando dataset convertido...")
 
-final_df.to_csv(
-    "data/UGR16-v3/UGR16-v3.csv",
-    index=False
-)
+final_df.to_csv("data/UGR16-v3/UGR16-v3.csv", index=False)
 
 print("Concluído!")
